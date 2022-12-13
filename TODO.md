4640a9e9349cc98a52786851ca082b5274de4d36
JonnerPaz <jonnerpazp@gmail.com>
2022-12-13 11:26:17 -0400

    Add styles to whole website

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
 * Try to add design system
+* Add width and height to img and icon
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // navbar toggle
 const burguerIcon = document.querySelector('.navbar__burguer-icon')
-const navbarComponent = document.querySelector('.navbar__mobile-component')
+const navbarComponent = document.querySelector('.navbar__burguer-icon-prompt')
 
 burguerIcon.addEventListener('click', () => {
-    navbarComponent.classList.toggle('navbar__mobile-component--hidden')
+    navbarComponent.classList.toggle('navbar__burguer-icon-prompt--hidden')
 })
 
